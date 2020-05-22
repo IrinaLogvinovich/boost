052f8e950fb8fe3b615b72afb8b2f74e6255ab54
dev16 <dev16@idea-labs.ru editor = "C:\Programs\Microsoft VS Code\Code.exe" --wait>
2020-05-22 14:09:14 +0300

    правки

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,16 +1,19 @@
 $(function(){
     $('.header__burger').on('click', function(evt){
         evt.preventDefault();
-        $('nav, .header__overlay').fadeToggle();
+        $('nav, .header__overlay').toggle(0);
+        $('nav').toggleClass('header__show');
         $('body').toggleClass('overflow')
     })
 
     $(window).on('resize', function(){
         if( $('.header').innerWidth() > 1600) {
             $('.header nav').css('display', 'flex');
+            $('.header nav').removeClass('header__show');
             $('body').removeClass('overflow')
         } else {
             $('.header nav, .header__overlay').css('display', 'none');
+            $('.header nav').removeClass('header__show');
         }
     })
 
--- src/js/modules/range.js
+++ src/js/modules/range.js
@@ -12,21 +12,21 @@ $(function(){
         $('.catalog__range-item')[index].noUiSlider.on('update', function (values, handle) {
             var value = values[handle];
             if (value.toString().length > 4) {
-               value = +value * 100;
+               value = Math.round(+value * 100);
                var thou = Math.floor(value/1000);
                var hand = value % 1000;
                console.log(hand);
                hand = (hand === 0) ? '000' : hand.toString()
                value = thou.toString() + "," + hand;
             } else {
-                value = +value * 100;
+                value = Math.round(+value * 100);
             }
             $('.catalog__range-item-value')[index].textContent = value;
         });
     });
 
-    $('.good__form-range-item').each(function(index){
-        noUiSlider.create($('.good__form-range-item')[index], {
+    $('.good__form-range-item--like').each(function(index){
+        noUiSlider.create($('.good__form-range-item--like')[index], {
             start: 0,
             step: 0.1,
             connect: 'lower',
@@ -35,10 +35,38 @@ $(function(){
                 max: 10
             }
         });
-        $('.good__form-range-item')[index].noUiSlider.on('update', function (values, handle) {
+        $('.good__form-range-item--like')[index].noUiSlider.on('update', function (values, handle) {
             var value = values[handle];
             $('.good__form-range-value')[index].textContent = Math.round(value*100);
             $('.good input[name="range"]')[index].value = Math.round(value*100);
+        });
+    });
+
+    $('.good__form-range-item--followers').each(function(index){
+        noUiSlider.create($('.good__form-range-item--followers')[index], {
+            start: 0,
+            step: 1,
+            connect: 'lower',
+            range: {
+                min: 0,
+                max: 150
+            }
+        });
+        $('.good__form-range-item--followers')[index].noUiSlider.on('update', function (values, handle) {
+            var value = values[handle];
+            if (value.toString().length > 4) {
+                var numberValue = +value * 100;
+                value = Math.round(+value * 100);
+                var thou = Math.floor(value/1000);
+                var hand = value % 1000;
+                console.log(hand);
+                hand = (hand === 0) ? '000' : hand.toString()
+                value = thou.toString() + "," + hand;
+             } else {
+                 value = Math.round(+value * 100);
+             }
+             $('.good__form-range-value')[index].textContent = value;
+            $('.good input[name="range"]')[index].value = Math.round(numberValue);
         });
     });
 })
